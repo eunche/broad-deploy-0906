b485a91965d764bdbd9f47d71da5f3bc7a3daeb0
eunchae619 <eunchae619@naver.com>
2020-09-06 19:06:48 +0900

    map 지도먹통 오류수정 테스트

--- static_local/map/js/backDetailToMap.js
+++ static_local/map/js/backDetailToMap.js
@@ -1,8 +1,4 @@
 const backDetailToMap = () => {
   detailContent.remove();
-  content.classList.remove("set_none");
   document.querySelector("header").innerHTML = mapHeader;
-  document
-    .querySelector("#map")
-    .setAttribute("style", `height:${viewHeight}px;`);
 };
--- static_local/map/js/backListToMap.js
+++ static_local/map/js/backListToMap.js
@@ -1,8 +1,4 @@
 const backListToMap = () => {
   newContent.remove();
-  content.classList.remove("set_none");
   header.innerHTML = beforeHeader;
-  document
-    .querySelector("#map")
-    .setAttribute("style", `height:${viewHeight}px;`);
 };
