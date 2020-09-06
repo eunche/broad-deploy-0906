006df769c3cd4a818d5a2bc8dbad43811863fe35
eunchae619 <eunchae619@naver.com>
2020-09-06 18:53:33 +0900

    map 지도먹통 오류수정 테스트

--- static/map/js/backListToMap.js
+++ static/map/js/backListToMap.js
@@ -2,4 +2,7 @@ const backListToMap = () => {
   newContent.remove();
   content.classList.remove("set_none");
   header.innerHTML = beforeHeader;
+  document
+    .querySelector("#map")
+    .setAttribute("style", `height:${viewHeight}px;`);
 };
