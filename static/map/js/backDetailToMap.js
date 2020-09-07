1f991d9c1d71bcc09ecb041f3e438e2633129ed4
eunchae619 <eunchae619@naver.com>
2020-09-07 21:31:25 +0900

    set_fixed로 인해 스크롤 안되는 현상 제거

--- static/map/js/backDetailToMap.js
+++ static/map/js/backDetailToMap.js
@@ -1,6 +1,5 @@
 const backDetailToMap = () => {
   detailContent.remove();
-  body.classList.add("set_fixed");
   content.classList.remove("set_none");
   document.querySelector("body").classList.remove("overflow_none");
   document.querySelector("header").innerHTML = mapHeader;
--- static/map/js/backListToMap.js
+++ static/map/js/backListToMap.js
@@ -1,6 +1,5 @@
 const backListToMap = () => {
   newContent.remove();
-  body.classList.add("set_fixed");
   content.classList.remove("set_none");
   header.innerHTML = beforeHeader;
   document.querySelector("body").classList.remove("overflow_none");
