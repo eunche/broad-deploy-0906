1ff3c434e9635efbdb240a6357dc1d225ad72539
eunchae619 <eunchae619@naver.com>
2020-09-08 16:36:27 +0900

    scrollavailable 제거

--- static_local/map/js/backDetailToMap.js
+++ static_local/map/js/backDetailToMap.js
@@ -3,12 +3,4 @@ const backDetailToMap = () => {
   content.classList.remove("set_none");
   document.querySelector("body").classList.remove("overflow_none");
   document.querySelector("header").innerHTML = mapHeader;
-  content.insertAdjacentHTML(
-    "afterbegin",
-    `
-    <div class="scroll_available"></div>
-  `
-  );
-  let scrollAvailable = document.querySelector(".scroll_available");
-  scrollAvailable.addEventListener("touchend", touchScrollAvailable, false);
 };
--- static_local/map/js/backListToMap.js
+++ static_local/map/js/backListToMap.js
@@ -3,12 +3,4 @@ const backListToMap = () => {
   content.classList.remove("set_none");
   header.innerHTML = beforeHeader;
   document.querySelector("body").classList.remove("overflow_none");
-  content.insertAdjacentHTML(
-    "afterbegin",
-    `
-    <div class="scroll_available"></div>
-  `
-  );
-  let scrollAvailable = document.querySelector(".scroll_available");
-  scrollAvailable.addEventListener("touchend", touchScrollAvailable, false);
 };
